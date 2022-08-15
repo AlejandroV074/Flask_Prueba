496732dc83084b817fedf9596bf43eeb3dbfb80d
AlejandroV074 <alejandrovargascuartas@gmail.com>
2022-08-15 02:45:45 -0500

    Se crea config

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app(enviroment):
+    app = Flask(__name__)    
+
+    app.config.from_object(enviroment)
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
+    return app
+
+enviroment = config["development"]
+if config_decouple("PRODUCTION", default=False):
+    enviroment = config["production"]
+app = create_app(enviroment)
 
 @app.route("/index.html")
 def index():
